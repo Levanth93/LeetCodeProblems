47327f90f4a9d6d42fddcf0da0c56dffe17b9fc9
Lavanth <176762554+Levanth93@users.noreply.github.com>
2024-12-26 09:55:26 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 6.65%] - LeetPush

--- 70-Climbing-Stairs.java
+++ 70-Climbing-Stairs.java
@@ -1,14 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1 || n==2){
-            return n;
-        }
-        int[] arr=new int[n+1];
-        arr[1]=1;
-        arr[2]=2;
-        for(int i=3;i<=n;i++){
-            arr[i]=arr[i-1]+arr[i-2];
-        }
-        return arr[n];
+        int[] dp=new int[n+1];
+        return climb(dp,n);
+    }
+
+    public static int climb(int[] dp,int n){
+        if(n==1 || n==2)
+        return n;
+
+        if(dp[n]!=0) return dp[n];
+
+        dp[n]=climb(dp,n-1)+climb(dp,n-2);
+
+        return dp[n];
     }
 }
